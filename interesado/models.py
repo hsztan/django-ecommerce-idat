4aad2c8e70f2048171e6b69d4818eadf48e043f2
Henry Nawrocki <hdev@sztanski.com>
2021-07-30 17:49:45 -0500

    devuelve error si no encuentra curso asociado con interesado

--- interesado/models.py
+++ interesado/models.py
@@ -8,7 +8,7 @@ class Interesado(models.Model):
     nombre = models.CharField(max_length=150, null=False, blank=False)
     celular = models.CharField(max_length=30, null=False, blank=False)
     correo = models.EmailField(null=False, blank=False, validators=[validate_email], unique=True)
-    curso = models.ForeignKey(Curso, null=True, on_delete=models.SET_NULL)
+    curso = models.ForeignKey(Curso, null=False, blank=False, on_delete=models.CASCADE)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- interesado/serializers.py
+++ interesado/serializers.py
@@ -1,8 +1,20 @@
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from rest_framework import serializers
 from .models import Interesado
+from curso.models import Curso
+from rest_framework.status import HTTP_404_NOT_FOUND
 
 
 class InteresadoSerializer(serializers.ModelSerializer):
+    
+    # def validate(self, attrs):
+    #     try:
+    #         print(attrs["curso"])
+    #     except:
+    #         raise ValidationError("hola lola")
+    #     return attrs
+        
+
     class Meta:
         model = Interesado
         fields = ['id', 'nombre', 'correo', 'celular', 'curso']
